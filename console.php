713af40a61d7b67dc385512b8aff0bea3f445261
Denis Glushkov <amkarovec@gmail.com>
2020-05-16 22:42:36 +0500

    Improve cli functionality

--- console.php
+++ console.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Usage:
+ * ./sf extName/actionName param1 paramN - params will passed as method params
+ * Or ./sf extName/actionName --param1=value --paramN=value - params will passed as GET params.
+ *    Also if method accepts params in long writings such as --param1=value will be passed to method params by their names.
+ *    Example: ./sd test/testme --value=2 --key=1 will launch class ConsoleTest from /ext/test/consoletest.class.php
+ *             with method actionTestme($key, $value) this way: (new ConsoleTest())->actionTestme(1,2); // not 2,1
+ */
 ini_set('display_errors', 1);
 define('SF_INADMIN', false);
 define('SF_INSITE', false);
@@ -23,17 +30,11 @@ if (empty($_SERVER['DOCUMENT_ROOT'])) {
 }
 
 include 'autoload.php';
-
 include 'config.php';
-include 'core/sfdb.class.php';
-include 'core/sfuser.class.php';
-include 'core/sfcore.class.php';
-include 'core/sfpage.class.php';
-include 'core/sfbuffer.class.php';
 
 SFDB::connect();
 
-cliParamsToGET();
+$sfArgv = cliParamsToGET();
 
 SFUser::login();
 SFCore::init();
@@ -46,37 +47,63 @@ if (count($job) !== 2) {
 $ext = $job[0];
 $action = 'action' . ucfirst($job[1]);
 $file = $_SERVER['DOCUMENT_ROOT'] . "/ext/$ext/console$ext.class.php";
-if (is_file($file)) {
-    include_once $file;
-    $class = "Console" . ucfirst($ext);
-    if (class_exists($class)) {
-        $handler = new $class();
-        if (method_exists($handler, $action)) {
-            $handler->$action();
-            exit;
-        } else {
-            echo "method $action not found\n";
-        }
-    } else {
-        echo "class $class not found\n";
-    }
-} else {
-    echo "extension $ext not found\n";
+if (!is_file($file)) {
+    sfCliExit(1, "extension $ext not found");
 }
+include_once $file;
+$class = "Console" . ucfirst($ext);
+if (!class_exists($class)) {
+    sfCliExit(1, "class $class not found");
+}
+if ($class instanceof SFConsoleBase) {
+    sfCliExit(1, "class $class is not console controller");
+}
+$handler = new $class();
+if (!method_exists($handler, $action)) {
+    sfCliExit(1, "method $action not found");
+}
+$paramValues = $sfArgv;
+$reflection = new ReflectionMethod($handler, $action);
+if ($actionParams = $reflection->getParameters()) {
+    if (count($paramValues) < count($actionParams)) {
+        foreach ($actionParams as $actionParam) {
+            $paramName = $actionParam->getName();
+            if (array_key_exists($paramName, $_GET)) {
+                $paramValues[$actionParam->getPosition()] = $_GET[$paramName];
+            }
+        }
+    }
+    if (count($paramValues) < count($actionParams)) {
+        sfCliExit(1, "not enough params");
+    }
+}
+$handler->$action(...$paramValues);
 
-function cliParamsToGET() {
+
+function cliParamsToGET()
+{
     global $argc, $argv;
     if ($argc == 1) {
         return;
     }
-    foreach (array_slice($argv, 2) as $arg) {
+    $sfArgv = array_slice($argv, 2);
+    foreach ($sfArgv as $index => $arg) {
         if (strpos($arg, '--') !== 0) {
-            echo 'error reading args';
-            die;
+            continue;
         }
         $tmp = explode('=', trim($arg, '-'));
         $key = $tmp[0];
         $value = $tmp[1];
         $_GET[$key] = $value;
+        unset($sfArgv[$index]);
     }
+    return $sfArgv;
+}
+
+function sfCliExit($code, $message = '')
+{
+    if ($message) {
+        echo "$message\n";
+    }
+    exit($code);
 }
